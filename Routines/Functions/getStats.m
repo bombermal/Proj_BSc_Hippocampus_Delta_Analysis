8a70a6cc2a94950a3c7cef1b569dcc41a44337e5
Ivan <ivanalisson@hotmail.com>
2022-02-03 23:28:57 -0300

    Update figure 3

---
+++ Routines/Functions/getStats.m
@@ -0,0 +1,26 @@
+function [ testName, p, h , r, b] = getStats(left, right, sigOrRank, statsOrCorr)
+    if statsOrCorr == "Statistic"
+        [h, ~, ~] = swtest([left, right]);
+        if h == 1
+            if sigOrRank == "Signrank" 
+                [ p, h ] = signrank(left, right);
+                testName = "Signrank";
+            else
+                [ p, h ] = ranksum(left, right);
+                testName = "Ranksum";
+            end
+        else
+            [ h, p ] = ttest(left, right);
+            testName = "Ttest";
+        end
+        r = '-';
+        b = '-';
+    else
+        coef = polyfit(left, right, 1);
+        b = coef(1);
+        [r, p] = corr(left', right', 'Type', 'Spearman');
+        testName = 'Spearman';
+        h = '-';
+    end
+    
+end
--- Routines/Functions/plotScatterNCorr.m
+++ Routines/Functions/plotScatterNCorr.m
@@ -1,10 +1,10 @@
-function plotScatterNCorr(x1, y1, x2, y2, speedthresh, speedLim, xLim, yLim, ttl, xlbl, ylbl, mrkSize)
-    plot(x1, y1, 'ok', 'markerfacecolor', 'k', 'markersize', mrkSize)
+function plotScatterNCorr(x1, y1, x2, y2, colors, speedthresh, speedLim, xLim, yLim, ttl, xlbl, ylbl, mrkSize)
+    plot(x1, y1, sprintf('o%s', colors(1)), 'markerfacecolor', colors(1), 'markersize', mrkSize)
     hold on
-    plot(x2, y2, 'or', 'markerfacecolor', 'r', 'markersize', mrkSize)
+    plot(x2, y2, sprintf('o%s', colors(2)), 'markerfacecolor', colors(2), 'markersize', mrkSize)
     % Speed treshold line
     if ~isempty(speedthresh)
-        plot([speedthresh, speedthresh], speedLim, 'k--')
+        plot([speedthresh, speedthresh], speedLim, sprintf('%s--', colors(1)))
     end
     title(ttl)
     xlabel(xlbl)
@@ -20,7 +20,7 @@ function plotScatterNCorr(x1, y1, x2, y2, speedthresh, speedLim, xLim, yLim, ttl
     coefMzAmp = polyfit(x1,  y1, 1); 
     coefWhAmp = polyfit(x2,  y2, 1);
 
-    plot(x1, polyval(coefMzAmp, x1),'k-','linewidth',2)
-    plot(x2, polyval(coefWhAmp, x2),'r-','linewidth',2)
+    plot(x1, polyval(coefMzAmp, x1), sprintf('%s-', colors(1)), 'linewidth', mrkSize)
+    plot(x2, polyval(coefWhAmp, x2), sprintf('%s-', colors(2)), 'linewidth', mrkSize)
     hold off
 end
